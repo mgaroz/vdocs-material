dd963bbea4ecaa819d577edbf2884c65bccf7d01
mgaroz <50783268+mgaroz@users.noreply.github.com>
2019-08-12 20:26:31 -0600

    Update .travis.sh

--- .travis.sh
+++ .travis.sh
@@ -43,25 +43,25 @@ set -e
 # EOM
 
 # Deploy documentation to GitHub pages
-if [ "$TRAVIS_BRANCH" == "master" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  REMOTE="https://${GH_TOKEN}@github.com/squidfunk/mkdocs-material"
-
-  # Set configuration for repository and deploy documentation
-  git config --global user.name "${GH_NAME}"
-  git config --global user.email "${GH_EMAIL}"
-  git remote set-url origin ${REMOTE}
+#if [ "$TRAVIS_BRANCH" == "master" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
+#  REMOTE="https://${GH_TOKEN}@github.com/squidfunk/mkdocs-material"
+#
+#  # Set configuration for repository and deploy documentation
+#  git config --global user.name "${GH_NAME}"
+#  git config --global user.email "${GH_EMAIL}"
+#  git remote set-url origin ${REMOTE}
 
   # Install Material, so we can use it as a base template and add overrides
-  python setup.py install
+#  python setup.py install
 
   # # Override theme configuration
   # sed -i 's/name: null/name: material/g' mkdocs.yml
   # sed -i 's/custom_dir: material/custom_dir: overrides/g' mkdocs.yml
 
   # Build documentation with overrides and publish to GitHub pages
-  mkdocs gh-deploy --force
-  mkdocs --version
-fi
+#  mkdocs gh-deploy --force
+#  mkdocs --version
+#fi
 
 # Remove overrides directory so it won't get included in the image
 # rm -rf overrides
@@ -80,7 +80,7 @@ docker build -t ${TRAVIS_REPO_SLUG} .
 docker run --rm -it -v $(pwd):/docs ${TRAVIS_REPO_SLUG} build --theme material
 
 # Push release to PyPI
-twine upload -u ${PYPI_USERNAME} -p ${PYPI_PASSWORD} dist/*
+#twine upload -u ${PYPI_USERNAME} -p ${PYPI_PASSWORD} dist/*
 
 # Push image to Docker Hub
 docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
